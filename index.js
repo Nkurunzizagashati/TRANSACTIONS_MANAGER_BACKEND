8ccb91a8b4b0692fdca5dabb46205b9b44c0cbbe
Fabricegashati <gashati.tech@gmail.com>
2025-01-14 20:44:50 +0200

    add functionality to initialize server connection

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import createDBConnection from './utils/db.js';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(
+	cors({
+		origin: ['http://localhost:5173'],
+		credentials: true,
+		methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+	})
+);
+
+// Connect to the Database
+
+createDBConnection();
+
+const PORT = process.env.PORT || 3001;
+
+try {
+	app.listen(PORT, () => {
+		console.log(`Server is running on port ${PORT}`);
+	});
+} catch (error) {
+	console.error(
+		`ERROR OCCURED WHILE INITIALIZING THE SERVER CONNECTION: ${error.message}`
+	);
+	process.exit(1);
+}

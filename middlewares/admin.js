7340c5345838f84afa5d5cd76f98e017aa1b1bf2
Fabricegashati <gashati.tech@gmail.com>
2025-01-14 21:53:22 +0200

    define validations for admin registration, login, and updating

---
+++ middlewares/admin.js
@@ -0,0 +1,116 @@
+const registerAdminValidator = {
+	name: {
+		notEmpty: {
+			errorMessage: 'Name should not be empty',
+		},
+		isString: {
+			errorMessage: 'Name should be a string',
+		},
+	},
+	email: {
+		notEmpty: {
+			errorMessage: 'Email should not be empty',
+		},
+		isEmail: {
+			errorMessage: 'Email should be a valid email',
+		},
+	},
+	password: {
+		notEmpty: {
+			errorMessage: 'Password should not be empty',
+		},
+		isString: {
+			errorMessage: 'Password should be a string',
+		},
+		isLength: {
+			options: {
+				min: 6,
+				max: 10,
+			},
+			errorMessage: 'Password should have 6 to 10 characters',
+		},
+	},
+};
+
+const loginAdminValidator = {
+	email: {
+		notEmpty: {
+			errorMessage: 'Email should not be empty',
+		},
+		isEmail: {
+			errorMessage: 'Email should be a valid email',
+		},
+	},
+	password: {
+		notEmpty: {
+			errorMessage: 'Password should not be empty',
+		},
+		isString: {
+			errorMessage: 'Password should be a string',
+		},
+		isLength: {
+			options: {
+				min: 6,
+				max: 10,
+			},
+			errorMessage: 'Password should have 6 to 10 characters',
+		},
+	},
+};
+
+const deleteAdminValidator = {
+	userId: {
+		in: ['params'],
+		notEmpty: {
+			errorMessage:
+				'You need to provide an ID for the user you want to delete',
+		},
+		isString: {
+			errorMessage: 'ID should be a string',
+		},
+	},
+};
+
+const updateAdminValidator = {
+	name: {
+		optional: { options: { nullable: true } },
+		notEmpty: {
+			errorMessage: 'Name should not be empty',
+		},
+		isString: {
+			errorMessage: 'Name should be a string',
+		},
+	},
+	email: {
+		optional: { options: { nullable: true } },
+		notEmpty: {
+			errorMessage: 'Email should not be empty',
+		},
+		isEmail: {
+			errorMessage: 'Email should be a valid email',
+		},
+	},
+	password: {
+		optional: { options: { nullable: true } },
+		notEmpty: {
+			errorMessage: 'Password should not be empty',
+		},
+		isString: {
+			errorMessage: 'Password should be a string',
+		},
+		isLength: {
+			options: {
+				min: 6,
+				max: 10,
+			},
+			errorMessage: 'Password should have 6 to 10 characters',
+		},
+	},
+};
+
+export {
+	registerAdminValidator,
+	loginAdminValidator,
+	deleteAdminValidator,
+	updateAdminValidator,
+};

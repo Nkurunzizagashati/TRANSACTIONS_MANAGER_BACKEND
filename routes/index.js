ff995f7a8e05a229dba7dd01c15d3af3a7f0a028
Fabrice <f.nkurunziz@alustudent.com>
2025-01-16 02:09:52 +0200

    define the account routes in the main routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import adminRouter from './admin.js';
+import accountRouter from './account.js';
 
 const router = express.Router();
 
 router.use('/admin', adminRouter);
+router.use('/account', accountRouter);
 
 export default router;

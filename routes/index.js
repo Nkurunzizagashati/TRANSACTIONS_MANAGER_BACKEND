fb9e2c76e825605a17ec628f9fecb6f7ca2e3ac2
Fabrice <f.nkurunziz@alustudent.com>
2025-01-17 23:07:59 +0200

    register category routes in the main routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import adminRouter from './admin.js';
 import accountRouter from './account.js';
+import categoryRouter from './category.js';
 
 const router = express.Router();
 
 router.use('/admin', adminRouter);
 router.use('/account', accountRouter);
+router.use('/category', categoryRouter);
 
 export default router;

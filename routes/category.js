9e111397a13311748403b950e5327c608f05c64e
Fabrice <f.nkurunziz@alustudent.com>
2025-01-17 23:07:22 +0200

    define the routes for getting and creating a category

---
+++ routes/category.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import { checkSchema } from 'express-validator';
+import { createCategoryValidator } from '../middlewares/category.js';
+import { createCategory } from '../controllers/category.js';
+
+const router = express.Router();
+
+router.get('/', getCategories);
+
+router.post(
+	'/create',
+	checkSchema(createCategoryValidator),
+	createCategory
+);
+
+export default router;

e1752167422c6c2fc3542b45cea26eda051f54a8
Fabrice <f.nkurunziz@alustudent.com>
2025-01-17 23:20:30 +0200

    define the account routes

--- routes/account.js
+++ routes/account.js
@@ -1,10 +1,14 @@
 import express from 'express';
-import { checkSchema } from 'express-validator.js';
+import { checkSchema } from 'express-validator';
 import {
 	createAccount,
 	getAccounts,
 	updateAccount,
 } from '../controllers/account.js';
+import {
+	createAccountValidator,
+	updateAccountValidator,
+} from '../middlewares/account.js';
 
 const router = express.Router();
 

79a041d7605e125c85267fe39864eddd5a4b7774
Fabrice <f.nkurunziz@alustudent.com>
2025-01-18 10:02:37 +0200

    add logic for creating a new category

--- controllers/category.js
+++ controllers/category.js
@@ -1,10 +1,10 @@
 import { matchedData, validationResult } from 'express-validator';
-import { getLoggedInUser } from '../utils/helpers';
-import Category from '../models/category';
+import { getLoggedInUser } from '../utils/helpers.js';
+import Category from '../models/category.js';
 
 const createCategory = async (req, res) => {
 	try {
-		const loggedInUser = getLoggedInUser(req, res);
+		const loggedInUser = await getLoggedInUser(req);
 		const adminId = loggedInUser.userId;
 
 		const result = validationResult(req);
@@ -15,18 +15,29 @@ const createCategory = async (req, res) => {
 		}
 
 		const data = matchedData(req);
+		console.log(data);
 
-		const isSubCategory = data.parentCategoryId;
-		if (isSubCategory) {
+		if (data.parentCategoryId) {
 			const parentCategory = await Category.findById(
 				data.parentCategoryId
 			);
-
 			if (!parentCategory) {
 				return res
 					.status(404)
 					.json({ message: 'Parent category not found' });
 			}
+		}
+
+		const categoryExists = await Category.findOne({
+			name: data.name,
+			userId: adminId,
+			parentCategoryId: data.parentCategoryId || null,
+		});
+
+		if (categoryExists) {
+			return res.status(400).json({
+				message: 'Category with the same name already exists',
+			});
 		}
 
 		const newCategory = await Category.create({
@@ -36,11 +47,32 @@ const createCategory = async (req, res) => {
 
 		return res.status(201).json({
 			message: 'Category created successfully',
-			category: newCategory,
+			category: await newCategory.populate([
+				'userId',
+				'parentCategoryId',
+			]),
 		});
+	} catch (error) {
+		const statusCode = error.message.includes('not authorized')
+			? 401
+			: 500;
+		return res.status(statusCode).json({ message: error.message });
+	}
+};
+
+const getCategories = async (req, res) => {
+	try {
+		const loggedInUser = await getLoggedInUser(req, res);
+		console.log(loggedInUser);
+
+		console.log(loggedInUser.userId);
+		const categories = await Category.find({
+			userId: loggedInUser.userId,
+		}).populate(['userId', 'parentCategoryId']);
+		return res.status(200).json({ categories });
 	} catch (error) {
 		res.status(500).json({ message: error.message });
 	}
 };
 
-export { createCategory };
+export { createCategory, getCategories };

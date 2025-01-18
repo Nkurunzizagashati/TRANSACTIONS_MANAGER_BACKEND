effdc0db73218f0ab84eb5266643feb06ebbd83d
Fabrice <f.nkurunziz@alustudent.com>
2025-01-18 14:21:38 +0200

    define category routes

--- routes/category.js
+++ routes/category.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import { checkSchema } from 'express-validator';
 import { createCategoryValidator } from '../middlewares/category.js';
-import { createCategory } from '../controllers/category.js';
+import {
+	createCategory,
+	getCategories,
+} from '../controllers/category.js';
 
 const router = express.Router();
 

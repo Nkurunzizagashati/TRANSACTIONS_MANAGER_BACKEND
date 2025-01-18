da69f6f6e531ecc976aace13d48bcb81d1122e76
Fabrice <f.nkurunziz@alustudent.com>
2025-01-18 14:23:15 +0200

    register account and transaction routes in the main routes

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import adminRouter from './admin.js';
 import accountRouter from './account.js';
 import categoryRouter from './category.js';
+import transactionRouter from './transaction.js';
 
 const router = express.Router();
 
 router.use('/admin', adminRouter);
 router.use('/account', accountRouter);
 router.use('/category', categoryRouter);
+router.use('/transaction', transactionRouter);
 
 export default router;
